f9a8b6aef02fdf535e1530d1b4befeee00d0108a
Emilio Olivastri <emilito.olivastri@gmail.com>
2026-06-01 11:25:54 +1000

    Updated script with correct formula for generating outier corrupted datasets

--- scripts/mainGenerator.py
+++ scripts/mainGenerator.py
@@ -2,15 +2,15 @@ import os
 
 
 def main():
-    n_loops = 80
-    dataset = "/home/slam-emix/Datasets/BACK_END/3D/KITTI_00/graph.g2o"
-    output_dir = "/home/slam-emix/Datasets/BACK_END/3D/KITTI_00/SPOILED_DATA/"
+    n_loops = 229
+    dataset = "/home/slamemix/Data/RobustSLAM/2D/FR079/graph.g2o"
+    output_dir = "/home/slamemix/Data/RobustSLAM/2D/FR079/SPOILED_DATA_EXTREME/"
     res_out = "/0"
     
     dir_idx = 10
     perc = 0.1
     for _ in range(10):
-        n = int(n_loops * perc)
+        n = int((perc * n_loops) / (1.0 - perc))
         output = output_dir + str(dir_idx) + res_out
 
         for idx in range(10):
